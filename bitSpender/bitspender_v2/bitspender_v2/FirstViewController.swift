63b029213e926dff692ceb39fb593b03dcd5871c
earyzhe <earyzhe@gmail.com>
2017-09-22 06:54:51 +0100

    Add files via upload

---
+++ bitSpender/bitspender_v2/bitspender_v2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  bitspender_v2
+//
+//  Created by Andrew Johnson on 21.09.17.
+//  Copyright © 2017 Rinson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
